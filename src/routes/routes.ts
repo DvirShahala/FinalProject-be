671f43b13996aca30ef10f591949261938827b97
Dvir Shahala <P0024269@ness-tech.co.il>
2020-09-17 12:12:29 +0300

    res status 200

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -36,7 +36,7 @@ router.use('/db', async (req, res) => {
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM test_table');
       const results = { 'results': (result) ? result.rows : null};
-      //res.render('pages/db', results );
+      res.render('pages/db', results );
       res.status(200);
       client.release();
     } catch (err) {
