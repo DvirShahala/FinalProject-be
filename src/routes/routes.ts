9a102ec278d707ef36cc09f8b3099d49282191cf
Dvir Shahala <P0024269@ness-tech.co.il>
2020-09-19 18:39:45 +0300

    update render

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -37,7 +37,7 @@ router.use('/db', async (req, res) => {
       const result = await client.query('SELECT * FROM test_table');
       const results = { 'results': (result) ? result.rows : null};
       //res.set('view engine', 'html');
-      res.render('pages/db', results );
+     // res.render('pages/db', results );
       client.release();
     } catch (err) {
       console.error(err);
