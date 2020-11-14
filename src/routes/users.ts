9be5d7fdafda96e7339f5ee87c60fc801897d5ca
Dvir Shahala <P0024269@ness-tech.co.il>
2020-11-14 13:42:04 +0200

    updates

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,65 +4,63 @@ import * as express from "express";
 import * as jwt from "jsonwebtoken";
 import * as fs from "fs";
 
-const cookieParser = require("cookie-parser");
 const router = express.Router();
 const RSA_PRIVATE_KEY = fs.readFileSync("src/routes/private.key");
 
-const options:any = {
-    url: process.env.DATABASE_URL,
-    type: 'postgres',
-    entities: [
-        "src/entity/**/*.ts"
-     ],
-    synchronize: true
+const options: any = {
+  url: process.env.DATABASE_URL,
+  type: 'postgres',
+  entities: [
+    "src/entity/**/*.ts"
+  ],
+  synchronize: true
 }
-
 
 // GET all users
 router.get('/', async (req, res) => {
 
-    await createConnection(options).then(async connection => {
+  await createConnection(options).then(async connection => {
 
-        let userRepository = connection.getRepository(User);
-        let loadedUsers = await userRepository.find();
+    let userRepository = connection.getRepository(User);
+    let loadedUsers = await userRepository.find();
 
-        res.send(loadedUsers);
-        await connection.close();
+    res.send(loadedUsers);
+    await connection.close();
 
-    }).catch(error => console.log(error));
+  }).catch(error => console.log(error));
 })
 
 // GET specific user
-router.get('/specific',(req,res)=> {
-    createConnection(options).then(async connection => {
-        
-        let userRepository = connection.getRepository(User);  
-        const loadedUser = await userRepository.find({ where: { email: req.query.email }});
-        await connection.close();
+router.get('/specific', (req, res) => {
+  createConnection(options).then(async connection => {
 
-        res.send(loadedUser);
+    let userRepository = connection.getRepository(User);
+    const loadedUser = await userRepository.find({ where: { email: req.query.email } });
+    await connection.close();
 
-    }).catch(error => console.log(error));
+    res.send(loadedUser);
+
+  }).catch(error => console.log(error));
 })
 
 // POST user
 router.post('/', async (req, res) => {
 
-    createConnection(options).then(async connection => {
+  createConnection(options).then(async connection => {
 
-        let user = new User();
-        user.email = req.body.email;
-        user.fullName = req.body.fullName;
-        user.password = req.body.password;
+    let user = new User();
+    user.email = req.body.email;
+    user.fullName = req.body.fullName;
+    user.password = req.body.password;
 
-        let userRepository = connection.getRepository(User);
+    let userRepository = connection.getRepository(User);
 
-        await userRepository.save(user);
-        await connection.close();
+    await userRepository.save(user);
+    await connection.close();
 
-        res.send(user);
+    res.send(user);
 
-    }).catch(error => console.log(error));
+  }).catch(error => console.log(error));
 })
 
 // POST login
@@ -87,7 +85,7 @@ router.post("/login", (req, res) => {
           subject: emailUser
         });
         res.status(200).json({
-          idToken: jwtBearerToken, 
+          idToken: jwtBearerToken,
           expiresIn: 120
         });
 
